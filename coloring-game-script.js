f6d8057359a6d140cea718bf06c4ae15ba566de2
MariaMia28 <maria.mihaita03@e-uvt.ro>
2024-03-10 01:21:26 +0200

    working palette, just one showing

--- coloring-game-script.js
+++ coloring-game-script.js
@@ -1,42 +1,126 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-const submitButton = document.getElementById('submitQuiz');
+document.addEventListener("DOMContentLoaded", function() {
+  const submitButton = document.getElementById("submitQuiz");
+  const colorPalette = document.getElementById("colorPalette");
+  const canvasContainer = document.getElementById("canvas-container");
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const checkboxes = document.querySelectorAll(".answer");
+  const selectedEmotions = [];
+  let paletteColors = [];
+  let isDrawing = false;
 
-checkboxes.forEach(checkbox => {
-  checkbox.addEventListener('change', function() {
-    const checkedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    
-    if (checkedCheckboxes.length === 3) {
-      submitButton.disabled = false; // Enable the button
-    } else {
-      submitButton.disabled = true; // Disable the button
-    }
+  submitButton.addEventListener("click", function() {
+      
+      document.getElementById("question").style.display = "none";
+      document.querySelectorAll("ul li").forEach(function(li) {
+          li.style.display = "none";
+      });
+      submitButton.style.display = "none";
 
-    if (checkedCheckboxes.length > 3) {
-      this.checked = false; // Prevent checkbox from being checked
-    }
+      
+      checkboxes.forEach(function(checkbox) {
+          if (checkbox.checked && selectedEmotions.length < 3) {
+              selectedEmotions.push(checkbox.id);
+          }
+      });
+
     
-  
+      if (selectedEmotions.length > 0) {
+          selectedEmotions.forEach(function(emotion) {
+              const color = getColorForEmotion(emotion);
+              paletteColors.push(color);
+              createPaletteColor(color);
+          });
+
+          canvasContainer.style.display = "block";
+          loadImage();
+      }
   });
+
+  
+  checkboxes.forEach(function(checkbox) {
+      checkbox.addEventListener("change", function() {
+          
+          const checkedCount = document.querySelectorAll(".answer:checked").length;
+          if (checkedCount > 3) {
+              checkbox.checked = false;
+          }
+      });
+  });
+
+  function getColorForEmotion(emotion) {
+      switch (emotion) {
+          case "yellow":
+              return "yellow";
+          case "purple":
+              return "purple";
+          case "blue":
+              return "blue";
+          case "green":
+              return "green";
+          case "red":
+              return "red";
+          case "orange":
+              return "orange";
+          case "grey":
+              return "grey";
+          case "pink":
+              return "pink";
+          default:
+              return "black";
+      }
+  }
+
+  function createPaletteColor(color) {
+      const paletteColor = document.createElement("div");
+      paletteColor.className = "palette-color";
+      paletteColor.style.backgroundColor = color;
+      paletteColor.addEventListener("click", function() {
+          if (paletteColors.includes(color)) {
+              context.fillStyle = color;
+          }
+      });
+      colorPalette.appendChild(paletteColor);
+  }
+
+  function loadImage() {
+      const image = new Image();
+      image.src = "images/fluture.jpg";
+      image.onload = function() {
+          const aspectRatio = image.width / image.height;
+          const canvasWidth = 700;
+          const canvasHeight = canvasWidth / aspectRatio;
+          canvas.width = canvasWidth;
+          canvas.height = canvasHeight;
+
+          const x = (canvasWidth - image.width) / 2;
+          const y = (canvasHeight - image.height) / 2;
+          context.drawImage(image, x, y, image.width, image.height);
+      };
+  }
+
+
+  canvas.addEventListener("mousedown", function(event) {
+      isDrawing = true;
+      draw(event.pageX - this.offsetLeft, event.pageY - this.offsetTop);
+  });
+
+  canvas.addEventListener("mousemove", function(event) {
+      if (isDrawing) {
+          draw(event.pageX - this.offsetLeft, event.pageY - this.offsetTop);
+      }
+  });
+
+  canvas.addEventListener("mouseup", function() {
+      isDrawing = false;
+  });
+
+  function draw(x, y) {
+      if (isDrawing) {
+          context.beginPath();
+          context.arc(x, y, 5, 0, Math.PI * 2);
+          context.fill();
+          context.closePath();
+      }
+  }
 });
-
-
-submitButton.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    quiz.style.display = 'none';
-    submitQuiz.style.display = 'none';
-  
-    canvasContainer.style.display = 'block';
-  
-    displayCanvas();
- });
-  
- 
-  
-  
-  
-
-
-
-  
-  
